88b98bc8d36381f9da0c8b756e7c71ba5f4a6a75
wiliender <wiliendersilva@gmail.com>
2024-01-16 00:21:41 -0300

    fix: correction from PathParam to PathVariable

--- src/main/java/com/wiliender/desafioanotaai/controllers/CategoryController.java
+++ src/main/java/com/wiliender/desafioanotaai/controllers/CategoryController.java
@@ -31,13 +31,13 @@ public class CategoryController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> update(@PathParam("id") String id, @RequestBody CategoryDTO categoryData){
+    public ResponseEntity<Category> update(@PathVariable("id") String id, @RequestBody CategoryDTO categoryData){
     Category updateCategory = this.service.update(id, categoryData);
     return ResponseEntity.ok().body(updateCategory);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Category> delete(@PathParam("id") String id){
+    public ResponseEntity<Category> delete(@PathVariable("id") String id){
         this.service.delete(id);
         return ResponseEntity.noContent().build();
     }
